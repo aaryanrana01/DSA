3871bd2696bdcf2f1e94a8a9fce74c4bbf2f5bb5
aaryanrana01 <139672521+aaryanrana01@users.noreply.github.com>
2026-07-18 23:55:40 +0530

    Create 1547. Minimum Cost to Cut a Stick.cpp

---
+++ DynamicProgramming/18July2026/1547. Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,51 @@
+// class Solution {
+// public:
+//     int solve(int i, int j,  vector<int>& cuts, vector<vector<int>>&dp){
+//         if(i > j) return 0;
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int mini = 1e9;
+//         for(int ind =i; ind<=j; ind++){
+//             int cost = cuts[j+1]-cuts[i-1] + solve(i, ind-1, cuts, dp) + solve(ind+1, j, cuts, dp);
+//             mini = min(mini, cost);
+//         }
+//         return mini;
+
+//     }
+//     int minCost(int n, vector<int>& cuts) {
+//         int c = cuts.size();
+//         cuts.push_back(n);
+//         cuts.push_back(0);
+//         sort(cuts.begin(), cuts.end());
+//         vector<vector<int>>dp(c+2, vector<int>(c+2, -1));
+
+//         return solve(1, c, cuts, dp);
+//     }
+// };
+
+
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int c = cuts.size();
+        cuts.push_back(n);
+        cuts.push_back(0);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>>dp(c+2, vector<int>(c+2, 0));
+
+        int mini = 1e9;
+        for(int i= c; i>=1; i--){
+            for(int j= 1; j<=c; j++){
+
+                if(i > j) continue;
+                int mini = 1e9;
+                for(int ind =i; ind<=j; ind++){
+                    int cost = cuts[j+1]-cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
+    }
+};
