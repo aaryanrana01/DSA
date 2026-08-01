52ae7ff75d47e82f4cdffd81e6b0b7ef361577ad
aaryanrana01 <139672521+aaryanrana01@users.noreply.github.com>
2026-08-02 03:51:03 +0530

    Greedy 2

---
+++ GreedyAlgorithm/2August2026/860. Lemonade Change.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+
+        int count5 = 0;
+        int count10 = 0;
+        int count20 = 0;
+
+        for(int i = 0; i<n; i++){
+            if(bills[i] == 5){
+                count5++;
+            }
+            else if(bills[i] == 10){
+                if(count5){
+                    count5--;
+                    count10++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(count10 && count5){
+                    count10--;
+                    count5--;
+                    count20++;
+                }
+                else if(count5 >= 3){
+                    count5 -= 3;
+                    count20++;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+};
